ea54ee1853331eb2629735e7d5f2b493a4b0a085
Damian Roginski <definitelynotdamian@gmail.com>
2018-11-07 13:21:46 -0500

    finished

---
+++ functions.js
@@ -0,0 +1,14 @@
+function HelloConsole() {
+  console.log("Hello, World!");
+}
+function popup() {
+  alert("Hello, World");
+}
+function sayHello() {
+  var p = document.getElementById("hello");
+  p.innerHTML ="hello, World";
+}
+function helloStranger() {
+  var answer = prompt("Please Enter Your Name");
+  alert("Hello " + answer);
+}
